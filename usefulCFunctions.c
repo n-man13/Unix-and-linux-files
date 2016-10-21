b812f9889ca2bf74543e5d7c43305275bad21d24
Nikhil Shah <email4nikhil@gmail.com>
2016-10-20 23:03:34 -0400

    Created readInput function in usefulCFunctions.c

---
+++ usefulCFunctions.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//reads input from standard input till finalChar is reached, then returns a pointer to the data
+char * readInput(char finalChar){
+  
+  char *str = malloc(sizeof(char));
+  *str = getchar();
+  char *pwork = str;
+  
+  while (*pwork != finalChar){
+    str = realloc(str,strlen(str)+1;
+    *pwork = getchar();
+    pwork++;
+  }
+
+}
